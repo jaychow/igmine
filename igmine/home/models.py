c2334e9bc23623c1f2dca62e951214243499d17a
jaychow <jay.chow@katanaconnect.com>
2015-09-22 09:59:10 -0700

    first project upload

---
+++ igmine/home/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+import datetime
+from django.utils import timezone
+
+class image(models.Model):
+    title = models.CharField(max_length=255)
+    user_id = models.IntegerField()
+    pub_date = models.DateTimeField('date_pubished')
